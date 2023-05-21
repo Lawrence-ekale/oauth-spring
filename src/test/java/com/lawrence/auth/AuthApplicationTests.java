dac92f1069b90299078c4085a7af8f1167a52a82
Lawrence-ekale <lawrenceekale2@gmail.com>
2023-05-21 16:32:59 +0300

    First commit to oauth repo

---
+++ src/test/java/com/lawrence/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawrence.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
